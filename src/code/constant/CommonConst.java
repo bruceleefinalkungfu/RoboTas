b994461c7561452ca36774e97407fc7ff6fa0219
bruceleefinalkungfu <trueindiantiger@gmail.com>
2019-03-03 01:06:34 +0530

    Changed constant variable name. Introduced another constant

--- src/code/constant/CommonConst.java
+++ src/code/constant/CommonConst.java
@@ -4,4 +4,12 @@ public class CommonConst {
 
 	public static final int DISPLAY_NUMBER_OF_BARCODE_RESULTS_ON_SCREEN = 5;
 	
+	/**
+	 * 
+	 * Set this value to 10 for testing purpose so 5000 second task is finished in 50 seconds 
+	 * 
+	 */
+	public static int MILLISECONDS_IN_ONE_SECOND							= 10;
+
+	public static final double EXTRA_BATTERY_PERCENTAGE_DRAINED_PER_KG 		= 2.0;
 }
