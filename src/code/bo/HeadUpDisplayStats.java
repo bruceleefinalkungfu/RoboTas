2db24fe79bcf03e46f2c7b0e9d34d8601f8908a9
bruceleefinalkungfu <trueindiantiger@gmail.com>
2019-03-03 15:42:50 +0530

    Added a battery shutdown flag Logger logs an exception as well

--- src/code/bo/HeadUpDisplayStats.java
+++ src/code/bo/HeadUpDisplayStats.java
@@ -9,9 +9,25 @@ public class HeadUpDisplayStats {
 	private volatile boolean isBatteryLow;
 	private volatile boolean isOverweight;
 	private volatile int batteryPercentage;
-
+	
+	private volatile Boolean isBatteryShutdown;
+	
 	public HeadUpDisplayStats(String robotName) {
 		this.robotName = robotName;
+		this.isBatteryLow = false;
+		this.isBatteryShutdown = false;
+		this.isOverweight = false;
+		this.batteryPercentage = 100;
+	}
+
+	public Boolean getIsBatteryShutdown() {
+		return isBatteryShutdown;
+	}
+
+	private void setIsBatteryShutdown(Boolean isBatteryShutdown) {
+		this.isBatteryShutdown = isBatteryShutdown;
+		if(isBatteryShutdown)
+			isBatteryShutdown.notifyAll();
 	}
 
 	public String getRobotName() {
@@ -22,7 +38,7 @@ public class HeadUpDisplayStats {
 		return isBatteryLow;
 	}
 
-	public void setBatteryLow(boolean isBatteryLow) {
+	private void setBatteryLow(boolean isBatteryLow) {
 		this.isBatteryLow = isBatteryLow;
 	}
 
@@ -41,6 +57,7 @@ public class HeadUpDisplayStats {
 	public void setBatteryPercentage(int batteryPercentage) {
 		this.batteryPercentage = batteryPercentage;
 		setBatteryLow(batteryPercentage < RobotConst.BATTERY_LOW_PERCENTAGE? true : false);
+		setIsBatteryShutdown(batteryPercentage <= RobotConst.ROBOT_SHUTDOWN_BATTERY_PERCENTAGE? true : false);
 	}
 
 }
--- src/code/util/LoggerUtil.java
+++ src/code/util/LoggerUtil.java
@@ -1,9 +1,22 @@
 package code.util;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 public class LoggerUtil {
 
+	static StringWriter sw = new StringWriter();
+	static PrintWriter pw = new PrintWriter(sw);
+	
 	public static void log(String msg) {
 		System.out.println(msg);
+	}
+
+	public static void log(Exception e) {
+		e.printStackTrace(pw);
+		String stackTrace = sw.toString(); // stack trace as a string
+		log(e.getMessage());
+		log(stackTrace);
 	}
 	
 }
